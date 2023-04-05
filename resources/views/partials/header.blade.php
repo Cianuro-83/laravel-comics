3e933f59d1bad7c63b159274eb5beeffb0736ef9
Cianuro-83 <buzzeosimone@yahoo.com>
2023-04-05 17:35:45 +0200

    TERMINATA HEADER

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,17 +3,74 @@
 </header> -->
 
 <header>
-    <div class="container flex">
-        <div class="logo">
-            <img class="logo-img" src={{ vite::asset ("resources/images/dc-logo.png") }} alt="" />
-        </div>
-        <div class="nav">
-            <ul>
-                <li>njk
+    <div class="container-fluid bg-primary ">
+        <div class="container scb-container-top">
+            <div class="scb-top">
+                <span class="text-uppercase text-white ">dc power visa</span>
+                <span class="text-uppercase text-white ms-3">additional dc site ⌄ </span>
 
-                </li>
-            </ul>
+            </div>
         </div>
+
+    </div>
+    <div class="container flex">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="logo">
+                <img class="logo-img" src={{ Vite::asset ("resources/images/dc-logo.png") }} alt="" />
+            </div>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">CHARACTERS </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">COMICS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">MOVIES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">TV</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">GAMES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">COLLECTIBLES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">VIDEOS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">FANS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">NEWS</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            SHOP
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">DROPDOWN 1</a></li>
+                        </ul>
+                    </li>
+
+                </ul>
+                <div class="scb-search">
+                    <span> Search</span>
+                    <img src="{{ Vite::asset ("resources/images/search.png") }}" alt="">
+                </div>
+            </div>
+    </div>
+    </nav>
     </div>
 </header>
 <main>
