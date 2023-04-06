afd6d143ab9a4bff15ee3a11b808a4e4b8bb9735
Cianuro-83 <buzzeosimone@yahoo.com>
2023-04-06 16:36:09 +0200

    FEAT: UP&LOWER FOOTER

---
+++ resources/views/partials/upFooter.blade.php
@@ -0,0 +1,68 @@
+<div class="up-footer">
+    <div class="container">
+        <div class="prova flex-up-footer">
+            <div class="col-2">
+                <ul>
+                    <li>
+                        <h3 class="list-title">dc comics</h3>
+                    </li>
+                    <li class="list-items">Characters</li>
+                    <li class="list-items">Comics</li>
+                    <li class="list-items">Movies</li>
+                    <li class="list-items">Tv</li>
+                    <li class="list-items">Games</li>
+                    <li class="list-items">Videos</li>
+                    <li class="list-items">News</li>
+                </ul>
+                <ul>
+                    <li>
+                        <h3 class="list-title shop">shop</h3>
+                    </li>
+                    <li class="list-items">Shop DC</li>
+                    <li class="list-items">Shop DC Collectibles</li>
+                </ul>
+            </div>
+            <div class="col-2">
+                <ul>
+                    <li>
+                        <h3 class="list-title">dc</h3>
+                    </li>
+                    <li class="list-items">Terms Of Use</li>
+                    <li class="list-items">Privacy policy (New)</li>
+                    <li class="list-items">Ad Choices</li>
+                    <li class="list-items">Advertising</li>
+                    <li class="list-items">Jobs</li>
+                    <li class="list-items">Subscriptions</li>
+                    <li class="list-items">Talent Workshops</li>
+                    <li class="list-items">CPSC Certificates</li>
+                    <li class="list-items">Ratings</li>
+                    <li class="list-items">Shop Help</li>
+                    <li class="list-items">Contact Us</li>
+                </ul>
+            </div>
+            <div class="col-2">
+                <ul>
+                    <li>
+                        <h3 class="list-title">sites</h3>
+                    </li>
+                    <li class="list-items">DC</li>
+                    <li class="list-items">MAD Magazine</li>
+                    <li class="list-items">DC Kids</li>
+                    <li class="list-items">DC Universe</li>
+                    <li class="list-items">DC Power Visa</li>
+                </ul>
+            </div>
+            <div class="col-6">
+                jfoiewhiof
+            </div>
+        </div>
+        <div class="row">
+
+            <div class="col-9">
+                <p class="scb-cookies">All sites Contents TM and &copy 202 DC Entertainment, unless otherwise <span
+                        class="text-primary"><a href="#">noted here</a></span>. All Right reserved. <br><span
+                        class="text-primary"><a href="#">Cookies Setting</a></span></p>
+            </div>
+        </div>
+    </div>
+</div>
