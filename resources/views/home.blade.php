931fd62616e06c9f44c7b4533a775404816fcf6e
Cianuro-83 <buzzeosimone@yahoo.com>
2023-04-06 18:05:54 +0200

    FEAT: fine esercizio base

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,14 +2,19 @@
 
 @section('content')
 
-<div>
+<div class="home py-3">
     <div class="container">
-        <h2>
-            ciao cianuro
-        </h2>
+        <div class="locandine pt-4">
+            @foreach ($comics as $comic)
+            <div>
+                <img src="{{$comic['thumb']}}" alt="">
+                <p>{{$comic ["title"]}}</p>
+            </div>
+            @endforeach
+        </div>
     </div>
-    <div class="container">
-        kljlkj
+    <div class="container text-center">
+        <button class="px-3 text-uppercase fw-bold bg-primary text-white">load more</button>
     </div>
 </div>
 
